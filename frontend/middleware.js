1881abf001bbe574fd339e8b41cda479817b7b0a
fiskryeziu <fiskryeziu@gmail.com>
2023-10-16 14:33:18 +0200

    feat: middleware for persistent user login

---
+++ frontend/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+    const path = request.nextUrl.pathname
+
+
+    const isPublic = path === '/login' || path === '/register' || path === '/'
+
+    const token = request.cookies.get('token')?.value || ''
+
+    if (isPublic && token) {
+        return NextResponse.redirect(new URL('/home', request.nextUrl))
+    }
+    if (!isPublic && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+
+
+export const config = {
+    matcher: ['/', '/login', '/profile', '/register'],
+}
