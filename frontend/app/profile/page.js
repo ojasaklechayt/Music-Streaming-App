e12c3f2b027a296a797adb3657ecb690380bac5c
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-10-16 14:41:13 +0530

    Profile Page

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -1,9 +1,51 @@
-import React from 'react'
+// pages/profile.js
+'use client'
+import { useEffect, useState } from 'react';
 
-const Page = () => {
+// Dummy user data for testing
+const dummyUserData = {
+    username: 'john_doe',
+    email: 'johndoe@example.com',
+    profilePicture: 'https://placekitten.com/200/200', // Replace with an actual image URL
+    bio: 'Hello, I am John Doe. This is my profile.',
+    createdPlaylists: ['Playlist 1', 'Playlist 2'], // Replace with actual playlist data
+    uploadedSongs: ['Song 1', 'Song 2'], // Replace with actual song data
+};
+
+export default function Profile() {
+    const [user, setUser] = useState(dummyUserData);
+
     return (
-        <div>Page</div>
-    )
+        <div className="bg-gradient-to-b from-[#181616] to-[#052A4D] min-h-screen flex items-center justify-center text-black">
+            <div className="bg-white p-4 rounded-lg shadow-md w-80">
+                <img
+                    src={user.profilePicture}
+                    alt={`${user.username}'s profile picture`}
+                    className="w-32 h-32 mx-auto rounded-full"
+                />
+                <h2 className="text-2xl font-semibold text-center">{user.username}</h2>
+                <p className=" text-center">{user.bio}</p>
+                <div className="mt-4">
+                    <h3 className="text-lg font-semibold">Email:</h3>
+                    <p>{user.email}</p>
+                </div>
+                <div className="mt-4">
+                    <h3 className="text-lg font-semibold">Created Playlists:</h3>
+                    <ul>
+                        {user.createdPlaylists.map((playlist, index) => (
+                            <li key={index}>{playlist}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="mt-4">
+                    <h3 className="text-lg font-semibold">Uploaded Songs:</h3>
+                    <ul>
+                        {user.uploadedSongs.map((song, index) => (
+                            <li key={index}>{song}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default Page
