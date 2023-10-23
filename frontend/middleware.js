fc35730bde3bb10e3616c029bc4aa62909ec2fd9
Bence Fekete <fekete965@gmail.com>
2023-10-23 16:50:55 +0100

    Update middleware to remove cookies from the request on logout

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,22 +1,31 @@
 import { NextResponse } from 'next/server'
+import { AUTH_TOKEN_KEY } from './utils/constants'
+
+const publicPaths = [
+    '/',
+    '/login',
+    '/register'
+]
 
 export function middleware(request) {
     const path = request.nextUrl.pathname
 
+    const isPublic = publicPaths.includes(path)
 
-    const isPublic = path === '/login' || path === '/register' || path === '/'
+    const token = request.cookies.get(AUTH_TOKEN_KEY)?.value ?? null
 
-    const token = request.cookies.get('token')?.value || ''
-
-    if (isPublic && token) {
-        return NextResponse.redirect(new URL('/home', request.nextUrl))
-    }
     if (!isPublic && !token) {
         return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+    if (path === '/logout') {
+        request.cookies.delete(AUTH_TOKEN_KEY)
+        NextResponse.redirect(new URL('/home', request.nextUrl))
+    } else {
+        NextResponse.redirect(new URL('/home', request.nextUrl))
     }
 }
 
 
 export const config = {
-    matcher: ['/', '/login', '/profile', '/register', '/home'],
+    matcher: ['/', '/login', '/profile', '/register', '/home', '/logout'],
 }
