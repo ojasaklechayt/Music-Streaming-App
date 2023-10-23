de5928c4fb4efc4d15303fdd9348124d169a0af5
Ham12-3 <mobolaji2309@gmail.com>
2023-10-23 09:23:04 +0100

    updated page UI and Navbar

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,29 +1,36 @@
-'use client'
-import Image from 'next/image';
-import Groovewave from '../public/groovewave.svg';
-import { FaBars } from 'react-icons/fa';
-import Disk from '../public/disk.svg';
-import Navbar from '@/components/navbar';
+"use client";
+import Image from "next/image";
+import Groovewave from "../public/groovewave.svg";
+import { FaBars } from "react-icons/fa";
+import Disk from "../public/disk.svg";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
-
   return (
-    <main className="bg-gradient-to-b from-[#181616] to-[#052A4D] flex min-h-screen flex-col items-center">
-      <Navbar />
-      <div className='flex md:flex-row justify-center md:gap-x-40 lg:gap-x-60 items-center min-w-full overflow-hidden min-h-full'>
-        <div className='text-center pt-40 md:pt-0 w-[100%] md:w-[50%] mb-10'>
-          <h1 className='font-inria text-[3rem] md:text-[4rem] lg:text-[5.5rem]'>
-            Groove<span className='text-[#1D60A0]'>wave</span>
-          </h1>
-          <p className='font-koulen text-[1rem] md:text-[1.1rem] lg:text-[1.6rem]'>
-          &quot;Ride the GrooveWave: Your Sound, Your Vibe, Your Way!&quot;
-          </p>
-          <p className='font-poppins text-[0.9rem] md:text-[0.9rem] lg:text-[1rem] px-[10%]'>Are you ready to dive into a world of limitless music possibilities? GrooveWave is your passport to a universe of sounds, rhythms, and melodies. Whether you&apos;re a passionate music lover, an avid collector, or just looking for your next favorite song, GrooveWave has you covered</p>
+    <>
+      <main className=" bg-gradient-to-b from-[#181616] to-[#052A4D] flex min-h-screen flex-col items-center">
+        <Navbar />
+        <div className="flex md:flex-row justify-around md:gap-x-40 lg:gap-x-60 items-center min-w-full overflow-hidden min-h-full">
+          <div className="relative   gap-y-5 bg-white/[.03] py-10 px-4 backdrop-blur-[3px]  pt-40 md:pt-0 w-[100%] md:w-[50%] mb-10 rounded-lg">
+            <h1 className="text-[#00008B]font-inria text-[3rem] md:text-[4rem] lg:text-[5.5rem]">
+              Groove<span className="text-[#1D60A0]">wave</span>
+            </h1>
+            <p className=" font-koulen text-[1rem] md:text-[1.1rem] lg:text-[1.6rem]">
+              &quot;Ride the GrooveWave: Your Sound, Your Vibe, Your Way!&quot;
+            </p>
+            <p className=" font-poppins text-[0.9rem] md:text-[0.9rem] lg:text-[1rem] ">
+              Are you ready to dive into a world of limitless music
+              possibilities? GrooveWave is your passport to a universe of
+              sounds, rhythms, and melodies. Whether you&apos;re a passionate
+              music lover, an avid collector, or just looking for your next
+              favorite song, GrooveWave has you covered
+            </p>
+          </div>
+          <div className="hidden md:block md:mr-[-45%] lg:mr-[-35%] animate-spin animate-spin-slow">
+            <Image src={Disk} alt="Disk" />
+          </div>
         </div>
-        <div className='hidden md:block md:mr-[-45%] lg:mr-[-35%] animate-spin animate-spin-slow'>
-          <Image src={Disk} alt='Disk' />
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,45 +1,67 @@
-'use client'
-import React, { useState } from 'react'
-import Image from 'next/image';
-import Groovewave from '../public/groovewave.svg';
-import { FaBars } from 'react-icons/fa';
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Groovewave from "../public/groovewave.svg";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-    return (
-        <div className='flex flex-row justify-between items-center min-w-full px-5 bg-transparent py-2'>
-            <Image src={Groovewave} alt='Navbar' />
-            <div>
-                <button className='hidden md:block bg-[#F5F5F5] text-black font-bold hover:bg-[#253646] hover:text-white py-2 px-5 mt-2 md:mt-0 rounded-xl'><a href='/login'>Login</a></button>
-                <div className="md:hidden relative inline-block text-left">
-                    <div>
-                        <button
-                            type="button"
-                            onClick={toggleMenu}
-                            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover-bg-gray-50"
-                            id="menu-button"
-                            aria-expanded={isMenuOpen}
-                            aria-haspopup="true"
-                        >
-                            <FaBars className={`-mr-1 h-5 w-5 text-gray-400 transform ${isMenuOpen ? 'rotate-180' : 'rotate-0'}`} />
-                        </button>
-                    </div>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <div className="flex flex-row justify-between items-center min-w-full px-5 bg-transparent py-2">
+      <Image
+        src={Groovewave}
+        alt="Navbar"
+        className="relative mr-2 mt-4 gap-y-5 bg-white/[.03] backdrop-blur-[3px] rounded-xl"
+      />
+      <div>
+        <button className=" text-white relative mr-2 gap-y-5 bg-white/[.03] backdrop-blur-[3px] text-xl uppercase rounded-xl hover:bg-[#253646] hover:text-white py-2 px-5 mt-2 md:mt-0 ">
+          <a href="/login">Login</a>
+        </button>
+        <div className="md:hidden relative inline-block text-left">
+          <div>
+            <button
+              type="button"
+              onClick={toggleMenu}
+              className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover-bg-gray-50"
+              id="menu-button"
+              aria-expanded={isMenuOpen}
+              aria-haspopup="true"
+            >
+              <FaBars
+                className={`-mr-1 h-5 w-5 text-gray-400 transform ${
+                  isMenuOpen ? "rotate-180" : "rotate-0"
+                }`}
+              />
+            </button>
+          </div>
 
-                    {isMenuOpen && (
-                        <div className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
-                            <div className="py-1" role="none">
-                                <button className="text-black text-center font-bold py-2 px-5 mt-2 md:mt-0 rounded-xl" role="menuitem" id="menu-item-3"><a href='/login'>Login</a></button>
-                            </div>
-                        </div>
-                    )}
-                </div>
+          {isMenuOpen && (
+            <div
+              className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+              tabIndex="-1"
+            >
+              <div className="py-1" role="none">
+                <button
+                  className="text-black text-center font-bold py-2 px-5 mt-2 md:mt-0 rounded-xl"
+                  role="menuitem"
+                  id="menu-item-3"
+                >
+                  <a href="/login">Login</a>
+                </button>
+              </div>
             </div>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
