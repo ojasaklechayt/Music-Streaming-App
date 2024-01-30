c85d7aed76ce87a1735714a75886a31061896cd8
Stephen Stolk <shikharburman2@gmail.com>
2024-01-31 00:21:31 +0530

    Updated variants file

---
+++ frontend/app/Motion/variants.js
@@ -0,0 +1,16 @@
+export const fadeIn =(duration,distance) => {
+  return{
+    hidden: {
+    opacity: 0,
+    y: distance,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: duration,
+      ease: [0.43, 0.13, 0.23, 0.96],
+    },
+  },
+}
+}
